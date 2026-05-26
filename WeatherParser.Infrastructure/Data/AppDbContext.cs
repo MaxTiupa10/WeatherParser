e84e244b13e06de504d1b1a06b25be962e6f82da
Maksym <maksimtupa3@gmail.com>
2026-05-26 23:20:57 +0300

    feat(infra): implement AppDbContext and WeatherRepository

---
+++ WeatherParser.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherParser.Domain.Entities;
+
+namespace WeatherParser.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<WeatherRecord> WeatherRecords => Set<WeatherRecord>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<WeatherRecord>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+
+            entity.Property(w => w.City)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(w => w.Temperature).HasPrecision(5, 2);
+            entity.Property(w =>w.Humidity).HasPrecision(5, 2);
+            entity.Property(w => w.WindSpeed).HasPrecision(5, 2);
+            entity.Property(w => w.Pressure).HasPrecision(5, 2);
+        });
+    }
+}
---
+++ WeatherParser.Infrastructure/Repositories/WeatherRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherParser.Domain.Entities;
+using WeatherParser.Domain.Interfaces;
+using WeatherParser.Infrastructure.Data;
+
+namespace WeatherParser.Infrastructure.Repositories;
+
+public class WeatherRepository : IWeatherRepository
+{
+    private readonly AppDbContext _appDbContext;
+    
+    public WeatherRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task SaveAsync(WeatherRecord weatherRecord)
+    {
+        await _appDbContext.WeatherRecords.AddAsync(weatherRecord);
+        await _appDbContext.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<WeatherRecord>> GetHistoryAsync(string city)
+    {
+        return await _appDbContext.WeatherRecords
+            .Where(w => w.City.ToLower() == city.ToLower())
+            .OrderByDescending(w => w.CapturedAt)
+            .ToListAsync();
+    }
+
+    public async Task<WeatherRecord> GetByIdAsync(Guid id)
+    {
+        return await _appDbContext.WeatherRecords
+            .FirstOrDefaultAsync(w => w.Id == id);
+    }
+    
+}
